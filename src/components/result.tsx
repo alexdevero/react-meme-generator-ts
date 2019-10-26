9730d2c5b06a18dacbca95cb6422a941c01b369b
Alex Devero <deveroalex@gmail.com>
2019-10-26 11:56:17 +0200

    Add comments to result component

--- src/components/result.tsx
+++ src/components/result.tsx
@@ -1,9 +1,12 @@
+// Import React
 import * as React from 'react'
 
+// Interface for Result component
 interface ResultInterface {
   resultContainerRef: React.RefObject<any>;
 }
 
+// Result component
 const Result = (props: ResultInterface) => {
   return (
     <div ref={props.resultContainerRef} className="result"></div>
